ae1f3bba8c31a73aa510906f7391fb3695add667
Benedict Kohler <bkohler@muhlenberg.edu>
2021-03-12 13:31:52 -0500

    Fixed Data Load methods

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,11 +1,6 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:flutter/services.dart';
-import 'package:nutri_calc/AddFormulaScreen.dart';
-import 'package:path/path.dart' as p;
+import 'dart:collection';
 import 'package:flutter/material.dart';
-import 'package:csv/csv.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:nutri_calc/DataHelper.dart';
 
 class HomeScreen extends StatefulWidget {
   static const id = 'homescreen';
@@ -14,67 +9,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool _isDataLoaded;
+  HashMap<String, List<dynamic>> _csvData;
+  DataHelper _dataHelper;
+
   @override
-  void initState() {
+  Future<void> initState() {
     super.initState();
+    _isDataLoaded = false;
+    _dataHelper = new DataHelper();
+    loadCSV();
   }
 
-  Future LoadCSV() async {
-    String csv = "";
-    try {
-      String data = await rootBundle.loadString('assets/data.csv');
-      List<List<dynamic>> rowsAsListOfValues =
-          const CsvToListConverter().convert(data);
-      debugPrint(data);
-      // rowsAsListOfValues[0].add('Random Value');
-      // csv = const ListToCsvConverter().convert(rowsAsListOfValues);
-    } catch (e) {
-      print('Error: $e');
-      rethrow;
-    }
-
-    /*Directory documents = await getApplicationDocumentsDirectory();
-    debugPrint(documents.path);
-    try {
-      File file = File('${documents.path}/data.csv');
-      await file.writeAsString(csv);
-    } catch (e) {
-      debugPrint('Error: $e');
-    }
-
-    File file = File('${documents.path}/data.csv');
-    file.readAsString().then((String text) {
-      debugPrint(text);
-    }).catchError((e) {
-      debugPrint(e);
-    });*/
+  void loadCSV() async {
+    List<List<dynamic>> csvList =
+        await _dataHelper.GetDataList(); // Get list of lists of csv data
+    _csvData =
+        _dataHelper.GetDataMap(csvList); // Convert list of lists to hashmap
+    setState(() {
+      _isDataLoaded = true; // data is loaded
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Home')),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  child: Text("Load CSV"),
-                  onPressed: LoadCSV,
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: RaisedButton(
-                    child: Text('Add New Formula'),
-                    onPressed: () =>
-                        Navigator.pushNamed(context, AddFormulaScreen.id),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ));
+      appBar: AppBar(title: Text('Home')),
+      body: Center(
+        child: !_isDataLoaded
+            ? CircularProgressIndicator()
+            : Text("Data has been loaded: " + _csvData.length.toString()),
+      ),
+    );
   }
 }
