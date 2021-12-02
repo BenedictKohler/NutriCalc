a5341c4456ceca8ae5e7e520c71571ae412912fb
Benedict Kohler <bkohler@muhlenberg.edu>
2021-12-02 17:14:01 -0500

    Update

--- lib/mobile.dart
+++ lib/mobile.dart
@@ -1,10 +1,19 @@
 import 'dart:io';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 Future<void> saveAndLauchFile(List<int> bytes, String fileName) async {
-  final path = (await getExternalStorageDirectory()).path;
+  /* final path = (await getExternalStorageDirectory()).path;
   final file = File('$path/$fileName');
   await file.writeAsBytes(bytes, flush: true);
-  OpenFile.open('$path/$fileName');
+  OpenFile.open('$path/$fileName'); */
+
+  final dir = await getApplicationDocumentsDirectory();
+  final fullpath = join(dir.path, fileName);
+
+  final file = File(fullpath);
+  await file.writeAsBytes(bytes, flush: true);
+  OpenFile.open(fullpath);
+
 }
